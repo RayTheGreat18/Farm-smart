b3140a9f633072a57cda47d72835af76fcd398e4
Ray Sakala <103606571+RayTheGreat18@users.noreply.github.com>
2024-09-02 07:41:03 +0200

    Add html, css and javascript, add slide images

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+}
+
+// Initialize the slideshow
+showSlides();
+
