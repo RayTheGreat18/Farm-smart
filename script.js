31e82d2e84461967fa3b7b062aeea0f83f803b42
Ray Sakala <103606571+RayTheGreat18@users.noreply.github.com>
2024-09-02 08:01:38 +0200

    update slideshow and add an image

--- script.js
+++ script.js
@@ -9,21 +9,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 let slideIndex = 0;
+showSlides();
 
 function showSlides() {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
+    
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
+    
     slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }    
+    if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
+    
     setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 }
 
-// Initialize the slideshow
-showSlides();
 
